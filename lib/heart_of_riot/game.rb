0990d43ffbed09cc4d4af9133836bedf3cbb1a74
James Liu <jliu@wifi-131-179-16-225.host.ucla.edu>
2014-06-30 13:25:49 -0700

    refactor since we won't instantiate "Game"

--- lib/heart_of_riot/game.rb
+++ lib/heart_of_riot/game.rb
@@ -1,17 +1,11 @@
 module HeartOfRiot
-  class Game
+  module Game
    VERSION = 'v1.3'
-   attr_accessor :games, :region, :summoner
-   def initialize region, summoner
-    @region = region
-    @summoner = summoner
-    get_games
-   end
 
-   def get_games
-     uri = "#{VERSION}/game/by-summoner/#{@summoner}/recent"
-     response = HeartOfRiot::get(@region, uri)
-     @games = response['games']
+   def self.get_recent region, summoner
+     uri = "#{VERSION}/game/by-summoner/#{summoner}/recent"
+     response = HeartOfRiot::get(region, uri)
+     games = response['games']
    end
   end
 end
